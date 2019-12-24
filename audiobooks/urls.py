622fe3749b45dc6be4b94232f585a63335afd3dd
Jelena Jovanovic <jeljov@gmail.com>
2019-12-24 21:49:46 +0100

    Complete Django project

--- audiobooks/urls.py
+++ audiobooks/urls.py
@@ -2,6 +2,21 @@ from django.urls import path
 
 from audiobooks import views
 
+app_name = 'audiobooks'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    # audiobooks/
+    # path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+
+    # audiobooks/5/
+    path('<int:audiobook_id>/', views.detail, name='detail'),
+
+    # audiobooks/5/review/
+    path('<int:audiobook_id>/review/', views.review, name='review'),
+
+    # audiobooks/5/updated/
+    # path('<int:audiobook_id>/updated/', views.detail_updated, name='detail_updated')
+    path('<int:pk>/updated/', views.DetailUpdatedView.as_view(), name='detail_updated')
+
 ]
--- audiobooks/views.py
+++ audiobooks/views.py
@@ -1,6 +1,77 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
-def index(request):
-    return HttpResponse("<h3>Welcome to the Django audiobooks app</h3>")
+from .models import Audiobook, Review
+
+
+class IndexView(ListView):
+    context_object_name = 'latest_audiobooks'
+    template_name = 'audiobooks/index.html'
+
+    def get_queryset(self):
+        return Audiobook.objects.order_by('-release_date')[:5]
+
+
+# v.1
+# def index(request):
+#     latest_books = Audiobook.objects.order_by('-release_date')[:5]
+#     context = {'latest_audiobooks' : latest_books}
+#     return render(request, 'audiobooks/index.html', context)
+
+
+
+def detail(request, audiobook_id):
+    audiobook = get_object_or_404(Audiobook, pk=audiobook_id)
+    context = {
+        'audiobook': audiobook,
+        'rating_options': Review.RATING_OPTIONS
+    }
+    return render(request, 'audiobooks/detail.html', context)
+
+
+# v.1
+# def detail(request, audiobook_id):
+#     try:
+#         audiobook = Audiobook.objects.get(pk=audiobook_id)
+#     except Audiobook.DoesNotExist:
+#         raise Http404(f"<h4>Could not retrieve audiobook with id {audiobook_id}</h4>")
+#
+#     return render(request, 'audiobooks/detail.html', {'audiobook': audiobook})
+
+
+
+def review(request, audiobook_id):
+
+    audiobook = get_object_or_404(Audiobook, pk=audiobook_id)
+
+    try:
+        rating = request.POST['rating']
+        comment = request.POST['comment']
+    except KeyError as err:
+        err_context = {
+            'audiobook':audiobook,
+            'err_msg': f'ERROR: {str(err)}'
+        }
+        return render(request, 'audiobooks/detail.html', err_context)
+
+    audiobook.review_set.create(rating=int(rating), comment=comment)
+    redicted_to_url = reverse('audiobooks:detail_updated', args=(audiobook_id,))
+    return HttpResponseRedirect(redirect_to=redicted_to_url)
+
+
+class DetailUpdatedView(DetailView):
+    model = Audiobook
+    template_name = 'audiobooks/detail_updated.html'
+
+
+# v.1
+# def detail_updated(request, audiobook_id):
+#     audiobook = get_object_or_404(Audiobook, pk=audiobook_id)
+#     sorted_reviews = audiobook.review_set.order_by('-timestamp')
+#     context = {
+#         'audiobook': audiobook,
+#         'sorted_reviews': sorted_reviews
+#     }
+#     return render(request, 'audiobooks/detail_updated.html', context)
